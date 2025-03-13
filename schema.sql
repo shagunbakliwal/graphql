d9a21aaa825dba2176c02a84f6bd14ae8c0e7e6e
Shagun Bakliwal <shagunbakliwal@gmail.com>
2025-03-13 23:23:15 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,5 @@
+INSERT INTO my_table (name, status, data) 
+VALUES ('John', 'Active', '{"key1": "value1", "key2": "value2"}');
+
+INSERT INTO my_table (name, status, data) 
+VALUES ('Jane', 'Inactive', '{"key1": "value3", "key2": "value4"}');
